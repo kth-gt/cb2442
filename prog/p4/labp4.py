1aa0aee6e9c1fd47a5cd7f44c7a122de3ba38d01
andand <doubleanders@gmail.com>
2024-10-03 08:54:09 +0200

    updated labp4.py

--- prog/p4/labp4.py
+++ prog/p4/labp4.py
@@ -3,11 +3,12 @@
 
 import numpy as np
 
-# Replace these with your names
+
+# Replace these with your names:
 authors = ['A. Student', 'B. Helper']
 
 
-# 'dist_matr' is set
+# 'dist_matr' is set. This is a 2-dimensional NumPy array
 dist_matr = np.array([[0, 0.1, 0.2, 0.3],
                       [0.1, 0, 0.2, 0.3],
                       [0.2, 0.2, 0, 0.3],
@@ -18,13 +19,14 @@ names_list = ['S1', 'S2', 'S3', 'S4']
 # This allows testing the code by running the script in the terminal: $ python3 labp4.py 
 # Keeping these unchanged, the output should be '(S4,(S3,(S1,S2)));' or '(S4,(S3,(S2,S1)));' or corresponding.
 
-def upgma(dist_matr, names_list):
+def upgma(dist_matr, names_list, print_details):
     # 'names_list' is copied into 'nwk_list'. To start with, 'nwk_list' will contain one sequence name per element, 
-    # but it will be updated in each iteration of the while-loop below.
+    # but it will be updated in each iteration of the while-loop below, such that each element will represent a cluster of sequences, written in Newick format.
+    # After the while-loop, only one element (cluster) should remain, with all sequences, representing the final tree in Newick format.
     nwk_list = names_list
 
-    # 'cluster_list' is generated. This is a list of lists. Each element represents a cluster, as a list of the 
-    # indices of the sequences of the cluster (0 for 'S1', 1 for 'S2', etc). To start with, each element has just one 
+    # 'cluster_list' is generated. This is a list of lists. Each element of the outer list is itself a list that represents a cluster, as a list of the 
+    # indices of the sequences of the cluster (0 for 'S1', 1 for 'S2', etc). To start with, each element of the outer list (i.e. cluster) has just one 
     # index in it, but it will be updated in each iteration of the while-loop below.
     cluster_list = []
     for i in range(len(names_list)):
@@ -34,37 +36,44 @@ def upgma(dist_matr, names_list):
     # but make changes to 'updated_dist_matr' in each iteration of the while-loop below.
     updated_dist_matr = dist_matr
 
-    # while-loop that in each iteration merges the pair of clusters with smallest average distance between objects
-    # until there is only one cluster.
+    # Here we print how 'updated_dist_matr' and 'nwk_list' look to start with, for your information.
+    if (print_details == True):
+            print("updated_dist_matr (before while loop):")
+            print(updated_dist_matr)
+            print("nwk_list (before while loop):")
+            print(nwk_list)
+            print("")
+
+    # While-loop that in each iteration merges the pair of clusters with smallest average distance between objects
+    # until there is only one cluster. 'cluster_list', 'nwk_list' and 'updated_dist_matr' will be updated in each iteration.
     while(len(cluster_list) > 1):
-        ### Find the pair of clusters in 'updated_dist_matr' with the smallest distance (we call these "winning clusters"
-        # in the comments below), and get their indices in 'updated_dist_matr'.
-        # One way could be to go through each row / column combination of 'updated_dist_matr' with a nested for loop.
+        ### Find the pair of clusters in 'updated_dist_matr' with the smallest distance (we call these "nearest clusters" 
+        # in the comments below). Save the indices of the two clusters in the objects that you name 'smallest_row' and 
+        # 'smallest_col'. Note that 'smallest_row' should be the smaller index of the two, otherwise the code below will not work.
+        # One approach could be to go through each row / column combination of 'updated_dist_matr' with a nested for loop.
         # Write code here:
 
 
         ### Update 'cluster_list'.
-        # Make a list 'in_new_cluster' by combining the lists of sequence indices (from 'cluster_list') of the two "winning clusters".
-        # Write code here:
+        # Here we make a list 'in_new_cluster' by combining the lists of sequence indices of the two "nearest clusters".
+        in_new_cluster = cluster_list[smallest_row] + cluster_list[smallest_col]
 
-
-        # Remove the elements of the two "winning clusters" in 'cluster_list' (if done in two steps, it may be wise to remove 
-        # the later element first, to not screw up the indexing).
-        # Write code here:
-
-
-        # Add the 'in_new_cluster' list as an element to the end of 'cluster_list'.
-        # Write code here:
-
+        # Here we remove the elements of the two "nearest clusters" in 'cluster_list'. We remove the element with the highest 
+        # index first, to avoid changing the index of the other element.
+        del(cluster_list[smallest_col])
+        del(cluster_list[smallest_row])
+        
+        # Here we add the 'in_new_cluster' list as an element to the end of 'cluster_list'.
+        cluster_list.append(in_new_cluster)
 
         ### Updaate 'nwk_list'.
-        # Combine the two "winning clusters" of 'nwk_list' in one string, separating them with a "," and adding a "(" before 
-        # and a ")" after. For example, combining 'S1' and 'S2' should give '(S1,S2)'.
+        # Combine the two "nearest clusters" of 'nwk_list' in one string, separating them with a "," and adding a "(" before 
+        # and a ")" after. Call the string object 'new_cluster_nwk'. For example, combining 'S1' and 'S2' should give '(S1,S2)'.
         # Write code here:
 
 
-        # Remove the elements of of the two "winning clusters" in 'nwk_list' (if done in two steps, it may be wise to remove 
-        # the later element first, to not screw up the indexing).
+        # Remove the elements of of the two "nearest clusters" in 'nwk_list'. If done in two steps, it may be wise to remove 
+        # the later element first, to not screw up the indexing.
         # Write code here:
 
 
@@ -73,24 +82,33 @@ def upgma(dist_matr, names_list):
 
 
         ### Update 'updated_dist_matr'.
-        # Remove the two rows and columns of 'updated_dist_matr' that correspond to the two "winning clusters".
-        # Write code here:
+        # Here we remove the two rows and columns of 'updated_dist_matr' that correspond to the two "nearest clusters".
+        updated_dist_matr = np.delete(updated_dist_matr, [smallest_row, smallest_col], 0) # removes rows
+        updated_dist_matr = np.delete(updated_dist_matr, [smallest_row, smallest_col], 1) # removes columns
 
-
-        # Add a new row of zeros and a new column of zeros to the "end" of 'updated_dist_matr' (the zeros will be replaced below).
-        # We've done it for you since it's a bit tricky.
-        updated_dist_matr = np.append(updated_dist_matr, np.zeros((1, updated_dist_matr.shape[1])), 0) # add row with zeros to the end (bottom)
-        updated_dist_matr = np.append(updated_dist_matr, np.zeros((updated_dist_matr.shape[0], 1)), 1) # add column with zeros to the end (right)
+        # Here we add a new row and a new column to the "bottom" and "right" of 'updated_dist_matr' where the distances between the newly formed 
+        # cluster and the other clusters will be inserted. To start with, the values will all be set to 0 but they will later be replaced with 
+        # calculated distances.
+        updated_dist_matr = np.append(updated_dist_matr, np.zeros((1, updated_dist_matr.shape[1])), 0) # adds row with zeros to the end (bottom)
+        updated_dist_matr = np.append(updated_dist_matr, np.zeros((updated_dist_matr.shape[0], 1)), 1) # adds column with zeros to the end (right)
         
-        # Calculate the distance between the new cluster and each other cluster, using the average method, 
-        # and insert these distances in the last row and column of 'updated_dist_matr'.
-        # The indices of the sequences of the new cluster are in 'in_new_cluster'. One approach could be to go through each element 
-        # of 'cluster_list', except the last, to get the indices of the sequences of the other clusters, and calculate the average of 
-        # these sequences' distances to the sequences of the new cluster, using the values in the original 'dist_matr'.
+        # Calculate the distance between the new cluster and the other clusters, using the average method, and insert these distances 
+        # in the last row and column of 'updated_dist_matr'. The indices of the sequences of the new cluster are in the list 'in_new_cluster' (as 
+        # well as in the last elememt of cluster_list). One approach could be to go through each element of 'cluster_list', except the last, to get 
+        # the indices of the sequences of the other clusters, and calculate the average of these sequences' distances to the sequences of the new 
+        # cluster, using the values in the original 'dist_matr'.
         # Write code here:
 
-            
-    ## After the while loop, all clusters should have been merged into one, and only one element should exist in 'nwk_list'. 
+
+        # Here we print 'updated_dist_matr' and 'nwk_list' after each iteration of the while loop, which might help you trouble shoot the code.
+        if (print_details == True):
+            print("updated_dist_matr:")
+            print(updated_dist_matr)
+            print("nwk_list:")
+            print(nwk_list)
+            print("")
+
+    ## After the while loop has finished, all clusters should have been merged into one, and only one element should exist in 'nwk_list'.
     # We convert this to a string and add a ";" to the end, to get proper Newick format. This string is then returned.
     nwk = nwk_list[0] + ';'
     return (nwk)
@@ -99,6 +117,6 @@ def upgma(dist_matr, names_list):
 # Will only be executed if this file is run directly
 # e.g. by running the command "python3 labp4.py"
 if __name__ == "__main__":
-    nwk = upgma(dist_matr, names_list)
+    nwk = upgma(dist_matr, names_list, print_details = True)
     print(nwk)
 
